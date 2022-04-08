6ad77290cc6656e861e87e8622ec0880fd0d87b2
Sanszi San <sichieesan101@gmail.com>
2022-04-08 10:08:58 +0800

    Change Test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,12 +1,12 @@
 const contractAddress = "0x9341fa24438C78C43847A4ce83c3913d41aeB2cd"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Starsier Club";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Starsier Club NFT Project is a decentralized';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const h1_presale_mint = "Pre-Sale Minting is Open";
+const h1_public_mint = "Public Minting is Open";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
